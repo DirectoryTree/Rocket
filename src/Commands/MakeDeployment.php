07bed48a0881232ede81b0151cd7583fafabd17b
Steve Bauman <steven_bauman@outlook.com>
2020-08-01 18:35:55 -0400

    Added deployment creator and command creator

--- src/Commands/MakeDeployment.php
+++ src/Commands/MakeDeployment.php
@@ -3,8 +3,92 @@
 namespace DirectoryTree\Rocket\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Composer;
+use DirectoryTree\Rocket\Deployments\DeploymentCreator;
 
 class MakeDeployment extends Command
 {
-    //
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'make:deployment {version : The version to run on}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new deployment file';
+
+    /**
+     * The deployment creator instance.
+     *
+     * @var DeploymentCreator
+     */
+    protected $creator;
+
+    /**
+     * The Composer instance.
+     *
+     * @var Composer
+     */
+    protected $composer;
+
+    /**
+     * Constructor.
+     *
+     * @param DeploymentCreator $creator
+     * @param Composer          $composer
+     *
+     * @return void
+     */
+    public function __construct(DeploymentCreator $creator, Composer $composer)
+    {
+        parent::__construct();
+
+        $this->creator = $creator;
+        $this->composer = $composer;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $name = ltrim($this->input->getArgument('name'), 'v');
+
+        $this->writeDeployment($name);
+
+        $this->composer->dumpAutoloads();
+    }
+
+    /**
+     * Write the deployment file to disk.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function writeDeployment($name)
+    {
+        $file = $this->creator->create(
+            $name, $this->getDeploymentPath()
+        );
+
+        $this->line("<info>Created Deployment:</info> {$file}");
+    }
+
+    /**
+     * Get deployment path.
+     *
+     * @return string
+     */
+    protected function getDeploymentPath()
+    {
+        return $this->laravel->basePath().DIRECTORY_SEPARATOR.'deployments';
+    }
 }
---
+++ src/Deployments/Deployment.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace DirectoryTree\Rocket\Deployments;
+
+abstract class Deployment
+{
+    //
+}
